88c3887977f52af45d9701a7f85058df94fde7c1
Hoyoung Jung <honux77@gmail.com>
2015-07-20 21:47:25 +0900

    private course

--- s3sync.sh
+++ s3sync.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 # aws s3 sync help 
-aws s3 sync ./ s3://awskr/ --exclude '.git*' --exclude '.DS_Store' --dryrun
-echo 'aws s3 sync ./ s3://awskr/ --exclude ".git*" --exclude ".DS_Store"'
+BUCKET=awskr
+DO=$1
+if [ $# -gt 1 ]; then
+	echo "USAGE: $0 [do]"
+	exit 1
+fi
+
+echo "aws s3 sync ./ s3://$BUCKET/ --exclude \".git*\" --exclude \".DS_Store\""
+if [[ $DO == "do" ]]; then
+echo "apply"
+aws s3 sync ./ s3://$BUCKET/ --exclude '.git*' --exclude '.DS_Store'
+else
+echo "dryrun"
+aws s3 sync ./ s3://$BUCKET/ --exclude '.git*' --exclude '.DS_Store' --dryrun
+fi
