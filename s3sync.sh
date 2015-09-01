2ccf1081930060486f769e3fbb186677fd30b148
Hoyoung Jung <honux77@gmail.com>
2015-09-01 11:28:03 +0900

    some fix

--- s3sync.sh
+++ s3sync.sh
@@ -7,11 +7,14 @@ if [ $# -gt 1 ]; then
 	exit 1
 fi
 
+echo "deleting .DS_Store"
+find . -type f -name .DS_Store -delete
+
 echo "aws s3 sync ./ s3://$BUCKET/ --exclude \".git*\" --exclude \".DS_Store\""
 if [[ $DO == "do" ]]; then
 echo "apply"
-aws s3 sync ./ s3://$BUCKET/ --exclude '.git*' --exclude '.DS_Store'
+aws s3 sync ./ s3://$BUCKET/ --exclude '.git*' --exclude '.DS_Store' --delete
 else
 echo "dryrun"
-aws s3 sync ./ s3://$BUCKET/ --exclude '.git*' --exclude '.DS_Store' --dryrun
+aws s3 sync ./ s3://$BUCKET/ --exclude '.git*' --exclude '.DS_Store' --delete --dryrun
 fi
